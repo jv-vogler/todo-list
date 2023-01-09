d14be5dd09142dfb169a3883b892b531a432c7e7
jv-vogler <jvsvogler@gmail.com>
2023-01-09 17:02:24 -0300

    Add global styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,41 @@
 import Header from './Header';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
-import { createGlobalStyle } from 'styled-components';
+import colors from '../constants/colors';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+  }
+  
+  body {
+    height: 100vh;
+    color: ${colors.SECONDARY};
+    background-color: ${colors.BACKGROUND};
+    font-family: Poppins, sans-serif;
+  }
+`;
+
+const StyledApp = styled.div`
+    display: flex;  
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+    gap: 30px;
+`
 
 function App() {
   return (
-    <>
+    <StyledApp>
+      <GlobalStyle />
       <Header />
       <TodoForm />
       <TodoList />
-    </>
+    </StyledApp>
   );
 }
 
